7a648dd86618ebd81282f738329224e1d3b6d41f
Daniel Mirmilshteyn <daniel.mirmilshteyn@outlook.com>
2015-10-31 18:49:42 -0400

    Added constants file

--- Follower/Follower.ino
+++ Follower/Follower.ino
@@ -1,6 +1,3 @@
-const int LINE_WIDTH = 0.05;
-const int WHEEL_RADIUS = 0.035;
-
 void setup() {
   // put your setup code here, to run once:
 
---
+++ Follower/Setup.ino
@@ -0,0 +1,19 @@
+/*******************
+ * Hardware Constants
+ *******************/
+#define AIN1 3
+#define AIN2 5
+#define BIN1 9
+#define BIN2 6
+
+/*******************
+ * Robot Constants
+ *******************/
+#define WHEEL_RADIUS 0.035
+
+/*******************
+ * Environment Constants
+ *******************/
+#define LINE_WIDTH 0.05
+#define MAP_WIDTH 5
+#define MAP_HEIGHT 5 
