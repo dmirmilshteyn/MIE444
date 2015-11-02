26ba66337e6211356ef62d10a0adebbf6e45b6b6
Daniel Mirmilshteyn <daniel.mirmilshteyn@outlook.com>
2015-11-02 14:37:02 -0500

    Removed unused function

--- Follower/Mapping.h
+++ Follower/Mapping.h
@@ -1,7 +1,6 @@
 #ifndef MAPPING_H
 #define MAPPING_H
 
-void InitializeMapping();
 int CalculateInternalPosition(int x, int y);
 bool AccessMapElement(int x, int y);
 void SetMapElement(int x, int y, bool value);
