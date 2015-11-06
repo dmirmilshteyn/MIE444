18a1419e58ad649c820fef635f30549247588bfb
Anatoly Makarov <anatoly_makarov@hotmail.com>
2015-11-05 21:01:25 -0500

    added PID control...modified constants

--- Follower/Constants.h
+++ Follower/Constants.h
@@ -16,8 +16,8 @@ const int BIN2 = 6;
 const int LINE_SENSOR_1 = 2; //digital
 const int LINE_SENSOR_2 = 4; //digital
 const int LINE_SENSOR_3 = 7; //digital
-const int LINE_SENSOR_4 = 8; //digital
-const int LINE_SENSOR_5 = 12; //digital
+const int LINE_FOLLOW_SENSOR_LEFT = 8; //digital
+const int LINE_FOLLOW_SENSOR_RIGHT = 12; //digital
 const int WALL_SENSOR_1 = 11; //digital
 
 const int IR_DETECTOR = 10; //digital
@@ -35,7 +35,7 @@ const float WHEEL_RADIUS = 0.035;
 const float LINE_WIDTH = 0.05;
 const int MAP_WIDTH = 5;
 const int MAP_HEIGHT = 5;
-const int MAP_RESOLUTION = 0.025;
+const float MAP_RESOLUTION = 0.025;
 
 
 /**********************************************
@@ -43,6 +43,14 @@ const int MAP_RESOLUTION = 0.025;
  **********************************************/
 
 const int CHECKPOINT_REACTION_DURATION = 5000;
+
+//PID
+const bool LINE_DETECTED = 1;
+const bool LINE_NOT_DETECTED = 0;
+const float Kp = 1;
+const float Ki = 0.1;
+const float Kd = 0.5;
+const int AVERAGE_MOTOR_SPEED = 130;//avg PWM for both motors. Might make this a variable in the future
 
 
 #endif
--- Follower/Follower.ino
+++ Follower/Follower.ino
@@ -3,15 +3,26 @@
 #include "Constants.h"
 #include "MotorController.h"
 #include "Mapping.h"
-bool checkPointFound = false;
-bool checkPointComplete = false;
-void checkPointHandle(int currentTime);
-void enableLED();
-void disableLED();
-int checkpointTime;
-int ledBlinkCount = 0;
-bool ledOn = false;
-int roundToHundreds(int num);
+
+
+/*************Checkpoint Variables and Functions***********/
+bool checkPointFound = false; //Once Robot finds the checkpoint, gets set to "true"
+bool checkPointComplete = false; //Once the checkPointHandle() is complete after CHECKPOINT_REACTION_DURATION time, gets set to "true"
+void checkPointHandle(int currentTime); //Function to handle the events that occur once checkpoint is found
+void enableLED(); //turns checkpoint found indicator LED on
+void disableLED(); //turns checkpoint found indicator LED off
+int checkpointTime; //time in millis() at which the checkpoint was found. Used for  checkPointHandle() timer.
+int ledBlinkCount = 0; //a counter to control the blinking of the LED
+bool ledOn = false; //variable stoes the state of the LED.
+
+/*************Lane following PID Variables and Functions***********/
+void followLane();
+float lastError;
+int integral;
+void driveMotorsPID(int speedOffset)
+
+/*************Helper functions***********/
+int roundToHundreds(int num); //rounds an integer to 100's
 
 
 void setup() {
@@ -22,7 +33,7 @@ void setup() {
   pinMode(BIN2, OUTPUT);
   pinMode(IR_DETECTOR, INPUT);
   pinMode(ANTENNA_LED, OUTPUT);
-
+  lastError = 0;
 
   Serial.begin(115200);
 
@@ -42,11 +53,14 @@ void loop() {
   checkPointHandle(currentTime);
 }
 
+
+/**************************************************************************************
+*********************************Checkpoint Functions**********************************
+***************************************************************************************/
 void enableLED() {
   digitalWrite(ANTENNA_LED, HIGH);
   Serial.println("ON");
   ledOn = true;
-
 }
 void disableLED() {
   digitalWrite(ANTENNA_LED, LOW);
@@ -78,14 +92,60 @@ void checkPointHandle(int currentTime) {
   }
   else if (ledOn) {
     disableLED();
-    checkPointComplete=true;
+    checkPointComplete = true;
     Serial.println("DONE");
   }
 }
 
+/**************************************************************************************
+*********************************Lane following Functions******************************
+***************************************************************************************/
+void followLane() {
+  int leftSensor = 1 - digitalRead(LINE_FOLLOW_SENSOR_LEFT);
+  int rightSensor = 1 - digitalRead(LINE_FOLLOW_SENSOR_RIGHT);
+  int derivative;
+  float currentError; //too far left is negative...too far right is positive
+  float controller;
+
+  if (leftSensor || rightSensor) {
+    if (leftSensor && rightSensor) {
+      if (lastError < 0) {
+        currentError = -(leftSensor + rightSensor);
+      }
+      else {
+        currentError = (leftSensor + rightSensor);
+      }
+    }
+    else if (leftSensor) {
+      currentError = -leftSensor;
+    }
+    else if (rightSensor) {
+      currentError = rightSensor;
+    }
+  }
+  else {
+    currentError = 0;
+  }
+  integral = integral+currentError;
+  derivative = currentError-lastError;
+
+
+  controller = Kp * currentError + Ki * integral + Kd * derivative;
+  driveMotorsPID(controller)
+
+  lastError = currentError;
+}
+void driveMotorsPID(int speedOffset){
+
+  analogWrite(AIN1, AVERAGE_MOTOR_SPEED+speedOffset);
+  analogWrite(BIN1, AVERAGE_MOTOR_SPEED-speedOffset);
+}
+
+/**************************************************************************************
+*************************************Helper Functions**********************************
+***************************************************************************************/
 int roundToHundreds(int num) {
   return round(num / 100) * 100;
-
 }
 
 
