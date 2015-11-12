cd6eafb79081381282d5e2999e18114ecfd86a39
Daniel Mirmilshteyn <daniel.mirmilshteyn@outlook.com>
2015-11-12 04:40:27 -0500

    Implemented some pathfinding helper functions

--- Follower/Pathfinder.cpp
+++ Follower/Pathfinder.cpp
@@ -8,6 +8,45 @@ void Pathfinder::Dispose() {
   delete[] closed_nodes;
 }
 
-float Pathfinder::CalculateDistanceBetween(Node nodeA, Node nodeB) {
-  return 0;
+bool Pathfinder::IsBlocked(int x, int y) {
+  int position = CalculateInternalPosition(x, y);
+  int offset = CalculateInternalOffset(x, y);
+
+  byte internal_value = closed_nodes[position];
+
+  return internal_value & (1 << offset);
 }
+
+void Pathfinder::MarkBlocked(int x, int y, bool value) {
+  int position = CalculateInternalPosition(x, y);
+  int offset = CalculateInternalOffset(x, y);
+
+  byte internal_value = closed_nodes[position];
+
+  if (value) {
+    internal_value = internal_value | 1 << offset;
+  } else {
+    internal_value = internal_value & ~(1 << offset);
+  }
+
+  closed_nodes[position] = internal_value;
+}
+
+const Position* Pathfinder::GetNeighbourPositions(Position position) {
+  Position *neighbourPositions = new Position[8];
+
+  // Top row
+  neighbourPositions[0] = Position(position.x - 1, position.y - 1);
+  neighbourPositions[1] = Position(position.x, position.y - 1);
+  neighbourPositions[2] = Position(position.x + 1, position.y - 1);
+  // Middle row
+  neighbourPositions[3] = Position(position.x - 1, position.y);
+  neighbourPositions[4] = Position(position.x + 1, position.y);
+  // Bottom row
+  neighbourPositions[5] = Position(position.x - 1, position.y + 1);
+  neighbourPositions[6] = Position(position.x, position.y + 1);
+  neighbourPositions[7] = Position(position.x + 1, position.y + 1);
+
+  return neighbourPositions;
+}
+
--- Follower/Pathfinder.h
+++ Follower/Pathfinder.h
@@ -4,25 +4,46 @@
 #ifndef PATHFINDER_H
 #define PATHFINDER_H
 
+struct Position {
+  int x;
+  int y;
+
+  Position(int x, int y): x(x), y(y) { }
+  Position();
+};
+
 struct Node {
   Node *parent;
-  
-  byte x;
-  byte y;
-  
+
+  Position position;
+
   int g;
   // int h; - always 0 for Dijkstra's algorithm, no need to waste memory for this
 };
 
-class Pathfinder { 
+class Pathfinder {
   public:
     void Initialize();
     void Dispose();
-  
+
   private:
     byte *closed_nodes;
-  
-    float CalculateDistanceBetween(Node nodeA, Node nodeB);
+
+    bool IsBlocked(int x, int y);
+    void MarkBlocked(int x, int y, bool value);
+    const Position* GetNeighbourPositions(Position position);
+
+    inline float CalculateDistanceBetween(Position positionA, Position positionB) {
+      return pow(positionB.x - positionA.x, 2) + pow(positionB.y - positionA.y, 2);
+    }
+
+    inline int CalculateInternalPosition(int x, int y) {
+      return (y * MAP_TILES_WIDTH + x) / 8;
+    }
+
+    inline int CalculateInternalOffset(int x, int y) {
+      return (y * MAP_TILES_WIDTH + x) % 8;
+    }
 };
 
 #endif
